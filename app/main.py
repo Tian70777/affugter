50e344d7beee921a85ec2e373b9204c5039a89c4
rgammelby <rab.gammelby@gmail.com>
2026-09-24 09:34:22 +0200

    correction

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from .shelly import check_shelly
 from .electricity import fetch_electricity_price
 from .database import get_daily_threshold, log_error
 from .controller import server_based_loop
-from app.dashboard import router as dashboard_router
+from .dashboard_router import router as dashboard_router
 
 """
 App lifespan
